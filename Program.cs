0cf3fc9ce79076da1e6d648455c8d0bd6c83dad6
Atanas Ivanov Ivanov <atanasivanov1969@gmail.com>
2021-03-30 02:46:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        public static int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+        public static object BinarySearch(int[] input, int key)
+        {
+            int min = 0;
+            int max = input.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == input[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < input[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return "Element not found.";
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinarySearch(array, 19));
+            Console.ReadLine();
+        }
+    }
+}
